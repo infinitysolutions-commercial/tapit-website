74dd9262c264ea3e9e2aa71d04d68912c7d268ff
Luca <lucadelc@gmail.com>
2021-01-13 15:48:59 +0100

    Aggiunta SideBar per i dispositivi mobili

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import Navbar from "./components/NavBar";
 import { BrowserRouter as Router } from 'react-router-dom';
+import Home from "./pages";
 
 function App() {
   return (
     <Router>
-      <Navbar />
+        <Home />
     </Router>
   );
 }
