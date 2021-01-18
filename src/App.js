cbcb654ba830428e89c3e98eb9912e8d28d74950
Luca <lucadelc@gmail.com>
2021-01-19 00:04:32 +0100

    Fix per abilitare alla release

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from "./pages";
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
         <Switch>
             <Route path="/" component={Home} exact />
         </Switch>
